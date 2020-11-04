0b0d1de52b3a19ff3dda173b080ac92a8c6d8701
HarshvardhanJha1 <46719471+HarshvardhanJha1@users.noreply.github.com>
2020-11-04 17:17:42 +0530

    Front End changes (#11)
    * Fixed title from Meklet Search to DNA Finder
    
    * Enter check for query length less than 9
    
    * Implement Check to only display queries of length greater than 9

--- search_client/static/main.js
+++ search_client/static/main.js
@@ -24,25 +24,43 @@ let handleSearch = (e) => {
     })
     .then((data) => {
       // Clear previous results
-      document.getElementsByClassName("search-results-container")[0].innerHTML =
-        "";
-      if (data.length == 0) {
-        document.getElementsByClassName(
-          "search-results-container"
-        )[0].innerHTML = "<h2>No Results</h2>";
+      document.getElementsByClassName("search-results-container")[0].innerHTML = "";
+      let qry=searchParams["query"].toString();
+      let count=0;
+      if(qry.length>=9)
+      {
+        if (data.length == 0) {
+          document.getElementsByClassName(
+            "search-results-container"
+          )[0].innerHTML = "<h2>No Results</h2>";
+        } else {
+          document.getElementsByClassName(
+            "search-results-container"
+          )[0].innerHTML = `Found ${data.length} similar DNA sequence(s) :<hr/>`;
+          data.forEach((result) => {
+            let resultItem = document.createElement("div");
+            let title = result[0];
+            let contents = result[1];
+            resultItem.innerHTML = `<a href="#"><h2>📂 ${title}</h2></a><p class="summary">${contents}</p><hr/>`;
+            if(contents.toString().length>=9)
+            {
+              count++;
+              document
+              .getElementsByClassName("search-results-container")[0]
+              .appendChild(resultItem);
+            }
+          });
+        }
       } else {
         document.getElementsByClassName(
           "search-results-container"
-        )[0].innerHTML = `Found ${data.length} similar DNA sequence(s) :<hr/>`;
-        data.forEach((result) => {
-          let resultItem = document.createElement("div");
-          let title = result[0];
-          let contents = result[1];
-          resultItem.innerHTML = `<a href="#"><h2>📂 ${title}</h2></a><p class="summary">${contents}</p><hr/>`;
-          document
-            .getElementsByClassName("search-results-container")[0]
-            .appendChild(resultItem);
-        });
+        )[0].innerHTML = "<h2>Invalid Query. Enter a query of length greater than 9</h2>";
+      }
+      if(count==0)
+      {
+        document.getElementsByClassName(
+          "search-results-container"
+        )[0].innerHTML = "<h2>No Results</h2>";
       }
     });
 };
